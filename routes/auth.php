96c3355cda01fa9e82ed2d5a019550bebe66b773
Wrseno <warsenosetyono@gmail.com>
2024-10-15 21:58:17 +0700

    feat: add header access control

--- routes/auth.php
+++ routes/auth.php
@@ -1,23 +1,13 @@
 <?php
+require_once "../helpers/HeaderAccessControl.php";
 require_once '../controllers/AuthController.php';
 require_once "../database/Database.php";
 
 session_start(); // Pastikan sesi dimulai di awal
 
-// Handle preflight request (OPTIONS)
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: http://localhost:3306"); // Ganti dengan origin frontend Anda
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Max-Age: 3600");
-    header("HTTP/1.1 204 No Content");
-    exit(0); // Stop eksekusi untuk request OPTIONS
-}
-
 $database = new Database();
-$db = $database->getConnection();
-$authController = new AuthController($db);
+$conn = $database->getConnection();
+$authController = new AuthController($conn);
 
 // Handle HTTP methods
 $request_method = $_SERVER["REQUEST_METHOD"];
--- routes/users.php
+++ routes/users.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "../helpers/HeaderAccessControl.php";
 require_once "../controllers/UserController.php";
 require_once "../database/Database.php";
 
