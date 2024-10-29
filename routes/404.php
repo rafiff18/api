cd1535cdf265135b08d30664f0d1b662ebc8e8bd
Wrseno <warsenosetyono@gmail.com>
2024-10-29 12:28:20 +0700

    feat: menambahkan routes untuk endpoint yang tidak ada

---
+++ routes/404.php
@@ -0,0 +1,6 @@
+<?php
+    require_once "../helpers/HeaderAccessControl.php";
+    require_once "../helpers/ResponseHelper.php";
+
+    response(false, 'Page not found', 'Page not found', null, 404);
+?>
